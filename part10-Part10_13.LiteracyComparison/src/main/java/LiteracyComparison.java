69a6a0d9064a10e9b8350ea82366bec8447267f7
rynefletcher <rynefletcher@pm.me>
2021-04-12 17:50:35 -0500

    Add files via upload

---
+++ part10-Part10_13.LiteracyComparison/src/main/java/LiteracyComparison.java
@@ -0,0 +1,19 @@
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class LiteracyComparison {
+    
+    public static void main(String[] args) {
+        try{
+            Files.lines(Paths.get("literacy.csv"))
+                    .map(x -> x.split(","))
+                    .sorted((x, y) -> x[5].compareTo(y[5]))
+                    .forEach(row -> System.out.println(row[3] + " (" + row[4] + "), " + row[2].split(" ")[1].trim() + ", " + row[5]));
+        }catch(IOException error){
+            System.out.println("Error reading the file: " + error.getMessage());
+        }
+    }
+}
